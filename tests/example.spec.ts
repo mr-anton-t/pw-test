9d5ba23f9e93ef8bdb6819a9f4d9520d9fd7b03a
antont <anton.tryfanau@gmail.com>
2024-09-03 12:34:27 +0200

    add test from documentation

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -6,7 +6,6 @@ test('Check screen size', async ({ page }, testInfo) => {
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 
-
   console.log("project name: ", testInfo.project.name)
 
   console.log('page.viewportSize() is ',  page.viewportSize());
@@ -14,4 +13,18 @@ test('Check screen size', async ({ page }, testInfo) => {
   console.log('testInfo.project.use.viewport is ',  testInfo.project.use.viewport)
 
   expect( page.viewportSize()).toMatchObject({width: 1400, height: 1300})
+});
+
+test('should inherit use options on context when using built-in browser fixture', async ({browser,}) => {
+  // test from https://playwright.dev/docs/test-use-options#explicit-context-creation-and-option-inheritance
+
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  await page.goto('https://playwright.dev/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+
+  expect(await page.evaluate(() => window.innerWidth)).toBe(1400);
+  await context.close();
 });
